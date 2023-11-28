5e5d5651728bf0559928ccb3288e15cdf413824e
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-11-28 12:13:52 +0500

    working2

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 
 const App = () => {
     return (
-        <Navbar/>
+        <Routes>
+            <Route path="/" element={<Navbar/>}>
+                <Route path="signin" element={<h1>This is a Sign in page</h1>}/>
+                
+                <Route path="signup" element={<h1>Sign Up page</h1>}/>
+            </Route>
+        </Routes>
     )
 
 }
