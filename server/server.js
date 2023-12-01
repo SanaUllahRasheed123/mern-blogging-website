dee652b25196c752570838767dcc9df0bcf98628
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-12-01 16:17:22 +0500

    BackendStarted

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config'
+
+const server = express();
+let PORT = 3000;
+
+server.use(express.json());
+
+mongoose.connect(process.env.DB_LOCATION,{
+    autoIndex:true
+})
+
+server.post("/signup", (req,res) => {
+
+    console.log(req.body)
+    res.json(req.body)
+})
+
+server.listen(PORT, ()=>{
+    console.log('listening on port ->' + PORT);
+})
