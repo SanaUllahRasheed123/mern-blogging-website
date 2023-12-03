e9305902ad0f45b724492ed6b1ba63aef0ad2fb1
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-12-03 23:20:07 +0500

    CompletedOnSignIn1

--- server/server.js
+++ server/server.js
@@ -85,38 +85,70 @@ server.post("/signup", (req,res) => {
     // return res.status(200).json({"status":"okay"})
 })
 
-server.post("/signin", (req,res)=>{
-    let {email,password} = req.body;
+// server.post("/signin", (req, res)=>{
+//     let {email,password} = req.body;
 
-  User.findOne({"personal_info.email" : email})
-  .then((user)=>{
-    if(!user){
-        return res.status(403).json({ "error":"Email not found"});
+//   User.findOne({"personal_info.email" : email})
+//   .then((user)=>{
+//     if(!user){
+//         return res.status(403).json({ "error":"Email not found"});
 
-    }
+//     }
 
-    bcrypt.compare(password,user.personal_info.password, (err,result)=>{
+//     bcrypt.compare(password, user.personal_info.password, (err,result) => {
 
-        if(err){
-            return res.status(403).json({ "error":"Error occured while login please try again "});
+//         if(err){
+//             return res.status(403).json({ "error":"Error occured while login please try again "});
+//         }
+//         if(!result){
+//             return res.status(403).json({"error":"Incorrect Password"})
+//         } else {
+//             return res.status(200).json(formatDatatoSend(user))
+//         }
+//     })
+
+//     // return res.json({"status":"got user document"})
+
+
+//   })
+
+//   .catch(err =>{
+//     console.log(err.message);
+//     return res.status(500).json({"error":err.message})
+//   })
+
+// })
+
+server.post("/signin",(req, res)=>{
+    let {email,password}= req.body;
+
+    User.findOne({"personal_info.email":email})
+    .then((user)=>{
+        if(!user){
+            return res.json({ "status":"Email not found" })
+
         }
-        if(!result){
-            return res.status(403).json({"error":"Incorrect Password"})
-        } else {
-            return res.status(200).json(formatDatatoSend(user))
-        }
+        
+        bcrypt.compare(password, user.personal_info.password, (err,result)=>{
+
+            if(err){
+            return res.status(403).json({"error":"Error occured while login in please try again"});
+
+            }
+            if(!result){
+                return res.status(403).json({"error":"Incorrect Password Umar Daraz bhai g"});
+
+            }
+            else{
+                return res.status(200).json(formatDatatoSend(user))
+            }
+        })
+
     })
-
-    return res.json({"status":"got user document"})
-
-
-  })
-
-  .catch(err =>{
-    console.log(err.message);
-    return res.status(500).json({"error":err.message})
-  })
-
+    .catch(err => {
+        console.log(err.message);
+        return res.status(500).json({ "error":err.message })
+    })
 })
 
 server.listen(PORT, ()=>{
