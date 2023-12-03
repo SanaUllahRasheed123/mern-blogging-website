3201f196439f657ae2964a7acd13922896273348
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-12-03 22:24:44 +0500

    CompletedOnSignIn

--- server/server.js
+++ server/server.js
@@ -85,6 +85,40 @@ server.post("/signup", (req,res) => {
     // return res.status(200).json({"status":"okay"})
 })
 
+server.post("/signin", (req,res)=>{
+    let {email,password} = req.body;
+
+  User.findOne({"personal_info.email" : email})
+  .then((user)=>{
+    if(!user){
+        return res.status(403).json({ "error":"Email not found"});
+
+    }
+
+    bcrypt.compare(password,user.personal_info.password, (err,result)=>{
+
+        if(err){
+            return res.status(403).json({ "error":"Error occured while login please try again "});
+        }
+        if(!result){
+            return res.status(403).json({"error":"Incorrect Password"})
+        } else {
+            return res.status(200).json(formatDatatoSend(user))
+        }
+    })
+
+    return res.json({"status":"got user document"})
+
+
+  })
+
+  .catch(err =>{
+    console.log(err.message);
+    return res.status(500).json({"error":err.message})
+  })
+
+})
+
 server.listen(PORT, ()=>{
-    console.log('listening on port ->' + PORT);
+    console.log('Server is running on port ->' + PORT);
 })
