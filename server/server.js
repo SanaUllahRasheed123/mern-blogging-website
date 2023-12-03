ae6925f3cc2a74f7e840abf86854b5f6f8868106
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-12-03 20:20:41 +0500

    BackendW12

--- server/server.js
+++ server/server.js
@@ -20,10 +20,10 @@ mongoose.connect(process.env.DB_LOCATION,{
 const generateUsername = async (email) => {
     let username = email.split("@")[0];
 
-    let usernameExists = await User.exists({"personal_info.username": username}).then((result)=>result)
+    let isUsernameNotUnique = await User.exists({"personal_info.username": username}).then((result) => result)
 
      
-    usernameExists ? username += nanoid():"";
+    isUsernameNotUnique ? username += nanoid() : "";
 
     return username;
      
@@ -50,7 +50,7 @@ server.post("/signup", (req,res) => {
         return res.status(403).json({"error":"Password should be 6 to 20 characters long with a numeric 1 lowercase and 1 uppercase letters"})
     }
 
-    bcrypt.hash(password,10, async(err,hashed_password) =>{
+    bcrypt.hash(password,10, async (err,hashed_password) =>{
         // console.log(hashed_password)
         let username = await generateUsername(email);
 
