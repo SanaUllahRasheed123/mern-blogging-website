ece791c2c4d73d4e7843d9aea9239795a81b8e9b
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-12-06 11:04:53 +0500

    Connection form to Server3

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -10,6 +10,10 @@ const UserAuthForm = ({type}) => {
     const handleSubmit = (e) =>{
 
         e.preventDefault();
+        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+
 
         //formData
         let form = new FormData(authForm.current);
@@ -19,7 +23,23 @@ const UserAuthForm = ({type}) => {
         for(let[key,value] of form.entries()){
             formData[key] = value;
         }
-        console.log(formData);
+        // console.log(formData);
+        //validating the date from frontend
+        let {fullname,email,password} = formData;
+   if(fullname){
+    if(fullname.length < 3){
+        return console.log({"error":"Fullname must be at least 3 letters long"})
+    }
+   }
+    if(!email.length){
+        return console.log({"error":"Enter Email "})
+    }
+    if(!emailRegex.test(email)){
+        return console.log({"error":"Email is Invalid"})
+    }
+    if(!passwordRegex.test(password)){
+        return console.log({"error":"Password should be 6 to 20 characters long with a numeric 1 lowercase and 1 uppercase letters"})
+    }
     }
   return (
     <AnimationWrapper keyValue={type}>
