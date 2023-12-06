865341afc569b397b158d12701cf04629cda7740
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-12-06 12:44:26 +0500

    Connection form to Server4

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,125 +1,144 @@
-import  { useRef } from 'react'
-import googleIcon from '../imgs/google.png'
-import InputBox from '../components/input.component'
-
-import {Link} from "react-router-dom"
-import AnimationWrapper from '../common/page-animation'
-
-const UserAuthForm = ({type}) => {
-    const authForm = useRef();
-    const handleSubmit = (e) =>{
-
-        e.preventDefault();
-        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
-let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+import { useRef } from "react";
+import googleIcon from "../imgs/google.png";
+import InputBox from "../components/input.component";
 
 
+import { Link } from "react-router-dom";
+import { Toaster, toast } from "react-hot-toast";
+import axios from "axios";
+import AnimationWrapper from "../common/page-animation";
 
-        //formData
-        let form = new FormData(authForm.current);
-        let formData = {};
-        // console.log(form)
+const UserAuthForm = ({ type }) => {
+  const authForm = useRef();
+  const userAuthThroughServer = (serverRoute,formData)=>{
 
-        for(let[key,value] of form.entries()){
-            formData[key] = value;
-        }
-        // console.log(formData);
-        //validating the date from frontend
-        let {fullname,email,password} = formData;
-   if(fullname){
-    if(fullname.length < 3){
-        return console.log({"error":"Fullname must be at least 3 letters long"})
+    console.log(import.meta.env.VITE_SERVER_DOMAIN + serverRoute,formData)
+
+    axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute,formData)
+    .then(({ data })=>{
+        console.log(data);
+    })
+    .catch(({})=>{
+        toast.error(respone.data.error)
+    })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let serverRoute = type == "sign-in" ? "/signin" : "/signup";
+
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+    //formData
+    let form = new FormData(authForm.current);
+    let formData = {};
+    // console.log(form)
+
+    for (let [key, value] of form.entries()) {
+      formData[key] = value;
     }
-   }
-    if(!email.length){
-        return console.log({"error":"Enter Email "})
+    // console.log(formData);
+    //validating the date from frontend
+    let { fullname, email, password } = formData;
+    if (fullname) {
+      if (fullname.length < 3) {
+        return toast.error("Fullname must be at least 3 letters long");
+      }
     }
-    if(!emailRegex.test(email)){
-        return console.log({"error":"Email is Invalid"})
+    if (!email.length) {
+      return toast.error("Enter Email ");
     }
-    if(!passwordRegex.test(password)){
-        return console.log({"error":"Password should be 6 to 20 characters long with a numeric 1 lowercase and 1 uppercase letters"})
+    if (!emailRegex.test(email)) {
+      return toast.error("Email is Invalid");
     }
+    if (!passwordRegex.test(password)) {
+      return toast.error(
+        "Password should be 6 to 20 characters long with a numeric 1 lowercase and 1 uppercase letters"
+      );
     }
+
+    userAuthThroughServer(serverRoute,formData);
+  };
   return (
     <AnimationWrapper keyValue={type}>
-        <section className='h-cover flex items-center justify-center'>
-<form ref={authForm} className='w-[80%] max-w-[400px]'>
-<h1 className='text-4xl font-gelasio capitalize text-center mb-24'>
+      <section className="h-cover flex items-center justify-center">
+        <Toaster />
+        <form ref={authForm} className="w-[80%] max-w-[400px]">
+          <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+            {type == "sign-in" ? "Welcome back" : "Join Us Today"}
+          </h1>
 
+          {type != "sign-in" ? (
+            <InputBox
+              name="fullname"
+              type="text"
+              placeholder="Full Name"
+              icon="fi-rr-user"
+            />
+          ) : (
+            ""
+          )}
 
-{type == "sign-in" ? "Welcome back" : "Join Us Today"}
+          <InputBox
+            name="email"
+            type="email"
+            placeholder="abc@email.com"
+            icon="fi-rr-envelope"
+          />
 
-</h1>
+          <InputBox
+            name="password"
+            type="password"
+            placeholder="Password"
+            icon="fi-rr-key"
+          />
 
-{
-    type != "sign-in" ?
-    
-    <InputBox
-        name="fullname"
-        type="text"
-        placeholder="Full Name"
-        icon="fi-rr-user"
-    />
-    : ""
+          <button
+            className="btn-dark center mt-14"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            {type.replace("-", " ")}
+          </button>
 
-}
+          <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+            <hr className="w-1/2 border-black" />
+            <p>or</p>
+            <hr className="w-1/2 border-black" />
+          </div>
 
-<InputBox
-        name="email"
-        type="email"
-        placeholder="abc@email.com"
-        icon="fi-rr-envelope"
-    />
+          <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+            <img src={googleIcon} className="w-5" />
+            Continue with Google
+          </button>
 
-<InputBox
-        name="password"
-        type="password"
-        placeholder="Password"
-        icon="fi-rr-key"
-    />
-
-    <button className='btn-dark center mt-14'
-    type='submit' onClick={handleSubmit}>
-        {type.replace("-"," ")}
-    </button>
-
-    <div className='relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold'>
-    <hr className='w-1/2 border-black'/>
-    <p>or</p>
-    <hr className='w-1/2 border-black'/>
-
-
-    </div>
-
-    <button className='btn-dark flex items-center justify-center gap-4 w-[90%] center'>
-    <img src={googleIcon} className='w-5'/>
-        Continue with Google
-    </button>
-
-    {
-        type = "sign-in" ?
-        <p className='mt-6 text-dark-grey text-xl text-center'>
-        Don't have an account ?
-        <Link to="/signup" className="underline text-black text-xl ml-1">
-        Join us today
-        </Link>
-        </p>
-        :
-
-        <p className='mt-6 text-dark-grey text-xl text-center'>
-        Already a member?
-        <Link to="/signin" className="underline text-black text-xl ml-1">
-        Sign in here
-        </Link>
-        </p>
-    }
-
-</form>
-    </section>
+          {
+            (type = "sign-in" ? (
+              <p className="mt-6 text-dark-grey text-xl text-center">
+                Don't have an account ?
+                <Link
+                  to="/signup"
+                  className="underline text-black text-xl ml-1"
+                >
+                  Join us today
+                </Link>
+              </p>
+            ) : (
+              <p className="mt-6 text-dark-grey text-xl text-center">
+                Already a member?
+                <Link
+                  to="/signin"
+                  className="underline text-black text-xl ml-1"
+                >
+                  Sign in here
+                </Link>
+              </p>
+            ))
+          }
+        </form>
+      </section>
     </AnimationWrapper>
-    
-  )
-}
+  );
+};
 
-export default UserAuthForm
+export default UserAuthForm;
