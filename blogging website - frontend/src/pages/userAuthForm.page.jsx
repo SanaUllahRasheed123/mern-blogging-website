5b12bec3d5a3d3b724e9e2178b3f1c574a331d9b
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-12-07 15:29:53 +0500

    w2

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -5,12 +5,21 @@ import InputBox from "../components/input.component";
 
 import { Link } from "react-router-dom";
 import AnimationWrapper from "../common/page-animation";
+import {Toaster,toast} from 'react-hot-toast';
 
 const UserAuthForm = ({ type }) => {
   const authForm = useRef();
+  
+  const userAuthThroughServer = (serverRoute,formData) => {
+
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    let serverRoute = type == "sign-in" ? "/signin" : "/signup";
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
 
     //formdata
     let form = new FormData(authForm.current)
@@ -20,14 +29,32 @@ const UserAuthForm = ({ type }) => {
       formData[key] = value;
     }
 
-    console.log(formData);
+    let { fullname , email, password} = formData;
+    //form validation
+    if(fullname){
+      if(fullname.length < 3){
+        return toast.error("Fullname must be at least 3 letters long")
+    }
+    }
+  if(!email.length){
+      return toast.error("Enter Email ")
+  }
+  if(!emailRegex.test(email)){
+      return toast.error("Email is Invalid")
+  }
+  if(!passwordRegex.test(password)){
+      return toast.error("Password should be 6 to 20 characters long with a numeric 1 lowercase and 1 uppercase letters")
+  }
+
+   userAuthThroughServer(serverRoute,formData)
+      axios.post()
   }
   
   return (
     <AnimationWrapper keyValue={type}>
       <section className="h-cover flex items-center justify-center">
         
-        
+        <Toaster/>
         <form ref={authForm} className="w-[80%] max-w-[400px]">
           <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
             {type == "sign-in" ? "Welcome back" : "Join Us Today"}
