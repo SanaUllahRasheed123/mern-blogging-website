d7defd5b070a30785e9ba58f5db4b68407a7bf56
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-12-17 10:42:20 +0500

    Saturday

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,82 +1,80 @@
-import { useRef } from "react";
 import googleIcon from "../imgs/google.png";
 import InputBox from "../components/input.component";
 
 
 import { Link } from "react-router-dom";
 import AnimationWrapper from "../common/page-animation";
-import {Toaster,toast} from 'react-hot-toast';
+import { useRef } from "react";
+import { Toaster, toast } from 'react-hot-toast'
 import axios from 'axios';
-import {storeInSession} from "../common/session";
-// In userAuthForm.page.jsx
-// import { storeInSession } from '/src/common/session.jsx';
-
-// import { storeInSession } from '../common/session';
-// import {UserContext} from '../App';
-// import { authWithGoogle } from '../common/firebase';
 
 const UserAuthForm = ({ type }) => {
-  const authForm = useRef();
 
-  // let { userAuth: { access_token}, setUserAuth} = useContext(UserContext)
-  
+  const authForm = useRef()
   const userAuthThroughServer = (serverRoute,formData) => {
-
-
-    // console.log(axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData))
-
-
-    axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
-    .then(({ data }) =>{
-      storeInSession("user", JSON.stringify(data))
-      console.log(sessionStorage)
+    
+    axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute , formData)
+    .then(({ data })=>{
+      console.log(data)
     })
-    .catch(({ response }) =>{
+    .catch(({ response })=>{
       toast.error(response.data.error)
     })
-  
+
   }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    let serverRoute = type == "sign-in" ? "/signin" : "/signup";
+    e.preventDefault()
+
+    let serverRoute = type == "sign-in" ? "/signin" : "/signup"
+
     let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
     let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
 
 
-    //formdata
-    let form = new FormData(authForm.current)
+    // formData
+
+    let form = new FormData(authForm.current);
     let formData = {};
 
     for(let [key,value] of form.entries()){
       formData[key] = value;
-    }
-
-    let { fullname , email, password} = formData;
-    //form validation
-    if(fullname){
-      if(fullname.length < 3){
-        return toast.error("Fullname must be at least 3 letters long")
-    }
-    }
-  if(!email.length){
-      return toast.error("Enter Email ")
-  }
-  if(!emailRegex.test(email)){
-      return toast.error("Email is Invalid")
-  }
-  if(!passwordRegex.test(password)){
-      return toast.error("Password should be 6 to 20 characters long with a numeric 1 lowercase and 1 uppercase letters")
   }
 
-   userAuthThroughServer(serverRoute,formData)
+        let {fullname,email,password} = formData
+        //form validation
+
+       if(fullname){
+        if(fullname.length < 3){
+          return toast.error("Fullname must be at least 3 letters long")
+      }
+       }
+      
+      if(!email.length){
+          return toast.error("Enter Email ")
+      }
+      
+      if(!emailRegex.test(email)){
+          return toast.error("Email is Invalid")
+      }
+      if(!passwordRegex.test(password)){
+          return toast.error("Password should be 6 to 20 characters long with a numeric 1 lowercase and 1 uppercase letters")
+      }
+
+      userAuthThroughServer(serverRoute , formData)
+
   }
+  
+  
+
+  
   
   return (
     <AnimationWrapper keyValue={type}>
       <section className="h-cover flex items-center justify-center">
+      <Toaster/>
         
-        <Toaster/>
+        
         <form ref={authForm} className="w-[80%] max-w-[400px]">
           <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
             {type == "sign-in" ? "Welcome back" : "Join Us Today"}
@@ -111,6 +109,7 @@ const UserAuthForm = ({ type }) => {
             className="btn-dark center mt-14"
             type="submit"
             onClick={handleSubmit}
+            
           
           >
             {type.replace("-", " ")}
