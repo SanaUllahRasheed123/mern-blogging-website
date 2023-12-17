b7f7c47600662da55f6e9fff796071cab1661967
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-12-17 21:34:39 +0500

    Sunday2

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -7,6 +7,7 @@ import AnimationWrapper from "../common/page-animation";
 import { useRef } from "react";
 import { Toaster, toast } from 'react-hot-toast'
 import axios from 'axios';
+import { storeInSession } from "../common/session";
 
 const UserAuthForm = ({ type }) => {
 
@@ -15,9 +16,10 @@ const UserAuthForm = ({ type }) => {
     
     axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute , formData)
     .then(({ data })=>{
-      console.log(data)
+      storeInSession("user", JSON.stringify(data))
+      console.log(sessionStorage)
     })
-    .catch(({ response })=>{
+    .catch(({ response }) => {
       toast.error(response.data.error)
     })
 
