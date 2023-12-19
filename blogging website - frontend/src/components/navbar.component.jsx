514215c9b382f720527ce98dfcdb0aa1f59d5ab5
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-12-19 23:44:08 +0500

    Ai is the future

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -36,7 +36,14 @@ const Navbar = () => {
 
       {
         access_token ?
-        "user is logged in"
+       
+        <>
+          <Link to="/dashboard/notification">
+              <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
+          <i className="fi fi-rr-bell text-2xl block mt-1"></i>
+              </button>
+          </Link>
+        </>
         :
         <>
         <Link className="btn-dark py-2" to="/signin">
@@ -61,3 +68,13 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+
+{/* <>
+<Link to="/dashboard/notification">
+    <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
+<i className="fi fi-rr-bell"></i>
+    </button>
+</Link>
+</> */}
